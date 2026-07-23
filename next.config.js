39d28a94fe0dc6543c5d08483686e219cf2d9b4a
AriefinNurHidayat <l200220031@student.ums.ac.id>
2026-07-23 08:18:14 +0700

    chore: remove static export config for opennext integration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',           // <--- Baris wajib untuk Cloudflare Pages Static
     images: {
         unoptimized: true,      // <--- Agar komponen next/image tidak error karena server optimization mati
     },
